b1273a0aa2bad5c5e08155cec74ba9125583a88e
Fabian Wünderich <fabian@wuenderich.de>
2025-02-17 01:12:08 +0100

    Use new os.Root structure to prevent any escape out of the web templates directory

--- internal/tmpl/tmpl_noembed.go
+++ internal/tmpl/tmpl_noembed.go
@@ -2,8 +2,15 @@
 
 package tmpl
 
-import "os"
+import (
+	"github.com/fanonwue/go-short-link/internal/util"
+	"os"
+)
 
 func initialize() {
-	templates = os.DirFS("./web")
+	webRoot, err := os.OpenRoot("./web")
+	if err != nil {
+		util.Logger().Fatalf("Failed to read web templates: %v", err)
+	}
+	templates = webRoot.FS()
 }
